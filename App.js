f5a6cac8b47b185093b11e8f06df9759dafd97ee
Maria Jose Jaramillo A <mariaj.jaramilloa@autonoma.edu.co>
2024-05-21 16:27:50 -0500

    Feature: Create the initial react native project

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { GestureHandlerRootView, PanGestureHandler } from 'react-native-gesture-handler';
+import { View, StyleSheet } from 'react-native';
 
-export default function App() {
+export default function App(){
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <PanGestureHandler onGestureEvent={() => console.log('Gesture detected!')}>
+        <View style={styles.box}/>
+      </PanGestureHandler>
+    </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'blue',
+    margin: 20,
   },
 });
+
